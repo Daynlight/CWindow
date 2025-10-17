3c362a0c0322d6ade08a56ed0b815affda084490
daynlight <danielstodulski000@gmail.com>
2025-10-18 00:38:28 +0200

    more window info

--- CWindow/Renderer/WindowData.h
+++ CWindow/Renderer/WindowData.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include <chrono>
 
 namespace CW::Renderer{
 enum WindowMode{
@@ -8,12 +10,28 @@ enum WindowMode{
 };
 
 struct WindowData{
+  bool should_close = false;
+  bool vsync = 0;
+  WindowMode window_mode = WindowMode::WINDOW;
+  std::string title = "Window";
+  bool is_focused = true;
+  bool is_minimize = false;
+  bool is_maximize = false;
+  bool is_cursor_visible = true;
+  bool is_resizing = false;
+  bool is_dragging = false;
+  bool is_mouse_over = false; 
+  bool is_keyboard_input_enabled = true;
+  float opacity = 1.0f;
   unsigned int width;
   unsigned int height;
   unsigned int x;
   unsigned int y;
-  bool should_close = true;
-  bool vsync = 0;
-  WindowMode window_mode = WindowMode::WINDOW;
+  std::string icon;
+  float aspect_ratio;
+  std::chrono::high_resolution_clock::time_point on_time;
+  unsigned int monitor;
+  unsigned int monitor_count;
+  std::chrono::duration<float> delta_time;
 };
-}
+};
