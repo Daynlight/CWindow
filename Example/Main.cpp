d97d2c7f495f9fa21692ee11ccfa85565a181014
daynlight <danielstodulski000@gmail.com>
2025-10-18 02:52:12 +0200

    Scroll

--- Example/Main.cpp
+++ Example/Main.cpp
@@ -81,8 +81,6 @@ int main(){
     auto new_time = std::chrono::high_resolution_clock::now();
     delta_time = new_time - last_time;
     last_time = new_time;
-
-    printf("%.f : %.f\n", window_renderer->getInputData()->scroll_x, window_renderer->getInputData()->scroll_y);
   };
 
   // clean up
