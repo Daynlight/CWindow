9a8c431766249bfe330b5c6b7aa2266011fcda57
daynlight <danielstodulski000@gmail.com>
2025-10-19 18:50:02 +0200

    Draw Shader

--- CWindow/Renderer/Shader/DrawShader.h
+++ CWindow/Renderer/Shader/DrawShader.h
@@ -17,15 +17,16 @@ private:
   bool is_compiled = false;
   std::vector<const CW::Renderer::Uniform*> uniforms;
 
+private:
+  void compile();
+  void destroy();
+
 public:
   DrawShader(const std::string& vertex, const std::string& fragment);
   ~DrawShader();
 
   void bind();
   void unbind();
-  
-  void compile();
-  void destroy();
   
   void setVertexShader(const std::string& shader);
   void setFragmentShader(const std::string& shader);
