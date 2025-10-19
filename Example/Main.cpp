e37c1b953420b3ac06c5a231014567ba9ec5bcf5
daynlight <danielstodulski000@gmail.com>
2025-10-19 22:32:58 +0200

    Movement

--- Example/Main.cpp
+++ Example/Main.cpp
@@ -20,6 +20,7 @@ std::function<void(CW::Renderer::iRenderer *window)> renderSettingsWindow = [](C
   glm::vec2 z = (*uniform)["z"]->get<glm::vec2>(); 
   int maxIter = (*uniform)["maxIter"]->get<int>();
   glm::vec3 colors = (*uniform)["colors"]->get<glm::vec3>();
+  colors /= 255;
 
   ImGui::Begin("Settings", nullptr);
 
@@ -30,19 +31,14 @@ std::function<void(CW::Renderer::iRenderer *window)> renderSettingsWindow = [](C
   ImGui::SliderFloat2("Z_0 Sidler", &z[0], -3, 3, "%.3f");
 
   ImGui::InputFloat3("colors", &colors[0], "%.3f");
-  if(ImGui::ColorPicker3("colors", &colors[0])){
-    for(int i = 0; i < 3; i++){
-      if(colors[0] >1) colors[0] /= 255;
-      colors[0] *= 255;
-    }
-  }
+  ImGui::ColorPicker3("colors", &colors[0]);
 
   ImGui::InputInt("MaxIter", &maxIter);
   ImGui::End();
 
   (*uniform)["z"]->set<glm::vec2>(z);
   (*uniform)["maxIter"]->set<int>(maxIter);
-  (*uniform)["colors"]->set<glm::vec3>(colors);
+  (*uniform)["colors"]->set<glm::vec3>(colors * 255.0f);
 };
 
 
@@ -72,8 +68,13 @@ int main(){
   (*uniform)["maxIter"]->set<int>(500);
   (*uniform)["colors"]->set<glm::vec3>({20.0f, 100.0f, 5.0f});
   (*uniform)["world_pos"]->set<glm::vec2>({0.0f, 0.0f});
-  (*uniform)["zoom"]->set<float>(1.0f);
+  (*uniform)["zoom"]->set<float>(3.0f);
+  (*uniform)["window_ratio"]->set<glm::vec2>({
+    window->getWindowData()->width,
+    window->getWindowData()->height
+  });
 
+  
   // create viewport mesh
   CW::Renderer::Mesh viewport = CW::Renderer::Mesh(
   {
@@ -95,17 +96,23 @@ int main(){
     viewport.render();
     malgenbrot->unbind();
 
+  (*uniform)["window_ratio"]->set<glm::vec2>({
+    window->getWindowData()->width,
+    window->getWindowData()->height
+  });
+
+
     if(window->getInputData()->right_mouse_button_is_down){
       (*uniform)["z"]->set<glm::vec2>({
-        2 * (window->getWindowData()->width / 2 - window->getInputData()->mouse_x) / window->getWindowData()->width, 
-        2 * (window->getWindowData()->height / 2 - window->getInputData()->mouse_y) / window->getWindowData()->height
+        3 * (window->getWindowData()->width / 2 - window->getInputData()->mouse_x) / window->getWindowData()->width, 
+        3 * (window->getWindowData()->height / 2 - window->getInputData()->mouse_y) / window->getWindowData()->height
       });
     }
 
     if(window->getInputData()->scroll_is_down){
       (*uniform)["world_pos"]->set<glm::vec2>({
-        last_world_pos.x + (window->getInputData()->mouse_x - last_mouse_pos.x) / (*uniform)["zoom"]->get<float>(), 
-        last_world_pos.y + (window->getInputData()->mouse_y - last_mouse_pos.y) / (*uniform)["zoom"]->get<float>()
+        last_world_pos.x - (window->getInputData()->mouse_x - last_mouse_pos.x) * (*uniform)["zoom"]->get<float>(),
+        last_world_pos.y + (window->getInputData()->mouse_y - last_mouse_pos.y) * (*uniform)["zoom"]->get<float>()
       });
     }
     else{
@@ -116,7 +123,7 @@ int main(){
 
     float zoom = (*uniform)["zoom"]->get<float>();
     zoom += window->getInputData()->scroll_y * scroll_sensitivity * zoom;
-    zoom = glm::clamp(zoom, 0.0001f, 10.0f);
+    zoom = glm::clamp(zoom, 0.000001f, 10.0f);
     (*uniform)["zoom"]->set<float>(zoom);
 
     gui->render();
--- Example/Mandelbrot.h
+++ Example/Mandelbrot.h
@@ -21,13 +21,18 @@ uniform int maxIter;
 uniform vec2 z;
 uniform vec2 world_pos;
 uniform float zoom;
+uniform vec2 window_ratio;
+
 
 void main() {
-    vec2 a1 = (gl_FragCoord.xy - vec2(800.0, 600.0) / 2  + world_pos) / vec2(800.0, 600.0)  * zoom;
+    float ratio = window_ratio.y / window_ratio.x;
+    vec2 a1 = ((gl_FragCoord.xy - window_ratio / 2) / window_ratio) * zoom + world_pos / window_ratio;
+    a1.y = a1.y * ratio;
+    
     vec2 c = z;
     if (z.x == 0.0f && z.y == 0.0f){
         a1 = z;
-        c = (gl_FragCoord.xy - vec2(800.0, 600.0) / 2  + world_pos) / vec2(800.0, 600.0)  * zoom;
+        c = a1;
     }
     vec2 a2 = a1;
     
