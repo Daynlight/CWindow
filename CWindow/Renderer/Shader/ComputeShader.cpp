db58bf28e60b2755fc86b8597604ed05658ca2a5
daynlight <danielstodulski000@gmail.com>
2025-10-20 21:11:08 +0200

    Compure Shader

--- CWindow/Renderer/Shader/ComputeShader.cpp
+++ CWindow/Renderer/Shader/ComputeShader.cpp
@@ -1,33 +1,11 @@
 #include "ComputeShader.h"
 
-CW::Renderer::ComputeShader::ComputeShader(const std::string &compute_shader, bool save_on_gup)
-  :compute_shader(compute_shader), save_on_gup(save_on_gup), is_compiled(false) { };
+CW::Renderer::ComputeShader::ComputeShader(const std::string &compute_shader)
+  :compute_shader(compute_shader), is_compiled(false) { };
 
 CW::Renderer::ComputeShader::~ComputeShader() {
   destroy();
 }
-
-void CW::Renderer::ComputeShader::run(std::vector<float> data, std::vector<float>* return_data) {
-  if(!is_compiled)
-    compile();
-    
-  glUseProgram(compiledShader);
-    
-  glGenBuffers(1, &SSBO);
-  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, SSBO);
-  glBufferData(GL_SHADER_STORAGE_BUFFER, data.size() * sizeof(glm::vec2), data.data(), GL_STATIC_DRAW);
-
-  glDispatchCompute(data.size(), 1, 1);
-
-  if (return_data != nullptr) {
-    glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-    glGetBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, data.size() * sizeof(float), return_data->data());
-  };
-
-  if (!save_on_gup) {
-    glDeleteBuffers(1, &SSBO);
-  }
-};
 
 void CW::Renderer::ComputeShader::compile() {
   GLuint computeShaderPart = glCreateShader(GL_COMPUTE_SHADER);
@@ -44,9 +22,15 @@ void CW::Renderer::ComputeShader::compile() {
 }
 
 void CW::Renderer::ComputeShader::destroy() {
-  glDeleteProgram(compiledShader);  
-  if (SSBO) 
+  if (SSBO){
     glDeleteBuffers(1, &SSBO);
-
+    SSBO = 0;
+  } 
+    
+  if(compiledShader){
+    glDeleteProgram(compiledShader);
+    compiledShader = 0;
+  }
+      
   is_compiled = false;
-};
+}
--- Example/ComputeShader/Main.cpp
+++ Example/ComputeShader/Main.cpp
@@ -4,62 +4,6 @@
 
 
 CW::Renderer::iRenderer* window = nullptr;
-CW::Gui::iGui* gui = nullptr;
-CW::Renderer::DrawShader* malgenbrot = nullptr;
-CW::Renderer::Uniform* uniform  = nullptr;
-
-
-const float scroll_sensitivity = 0.02f; 
-const float sensitivity = 20.0f;
-glm::vec2 last_world_pos;
-glm::vec2 last_mouse_pos;
-bool animation = false;
-const float zoom_speed = 0.005;
-float current_zoom_speed = 0.005;
-
-
-
-std::function<void(CW::Renderer::iRenderer *window)> renderSettingsWindow = [](CW::Renderer::iRenderer *window){
-  glm::vec2 z = (*uniform)["z"]->get<glm::vec2>(); 
-  int maxIter = (*uniform)["maxIter"]->get<int>();
-  glm::vec3 colors = (*uniform)["colors"]->get<glm::vec3>();
-  colors /= 255;
-
-  ImGui::Begin("Settings", nullptr);
-
-  if(window->getWindowData()->delta_time >= 0.0f) 
-    ImGui::Text("FPS: %.f", 1.0f / window->getWindowData()->delta_time);
-
-  ImGui::InputFloat2("Z_0", &z[0], "%.3f");
-  ImGui::SliderFloat2("Z_0 Sidler", &z[0], -3, 3, "%.3f");
-
-  ImGui::InputFloat3("colors", &colors[0], "%.3f");
-  ImGui::ColorPicker3("colors", &colors[0]);
-
-  ImGui::InputInt("MaxIter", &maxIter);
-  
-  if(ImGui::Button("Animation")) 
-    animation = !animation;
-
-  ImGui::End();
-
-  if(animation){
-    if((*uniform)["zoom"]->get<float>() < 0.002) 
-      current_zoom_speed = -1 * (zoom_speed);
-
-    if((*uniform)["zoom"]->get<float>() > 3)
-      current_zoom_speed = (zoom_speed);
-     
-    (*uniform)["zoom"]->set<float>((*uniform)["zoom"]->get<float>() - (*uniform)["zoom"]->get<float>() * current_zoom_speed);
-  }
-
-  (*uniform)["z"]->set<glm::vec2>(z);
-  (*uniform)["maxIter"]->set<int>(maxIter);
-  (*uniform)["colors"]->set<glm::vec3>(colors * 255.0f);
-};
-
-
-
 
 
 int main(){
@@ -67,89 +11,20 @@ int main(){
   
   // init window and renderer
   window->setVsync(0);
-  window->setWindowTitle("Malgenbrota and Julia");
+  window->setWindowTitle("Compute Shader");
   
-  // init gui and add Settings Window
-  gui = new CW::Gui::Gui(window);
-  gui->addWindow("Settings", renderSettingsWindow);
+
+  CW::Renderer::ComputeShader* compute = new CW::Renderer::ComputeShader(ComputeShader::compute);
+
+  std::vector<float> data = {2, 1, 2, 3, 4, 5};
+  compute->run<float>(data, data.size()); // each thread one value
+  data = compute->get<float>();
   
-  // create uniform and malgenbrota shader
-  uniform = new CW::Renderer::Uniform();
-  malgenbrot = new CW::Renderer::DrawShader(Mandelbrot::vertex, Mandelbrot::fragment);
-  malgenbrot->getUniforms().emplace_back(uniform);
-
-  // uniform default values
-  (*uniform)["z"]->set<glm::vec2>({0.394f, 0.355f});
-  (*uniform)["maxIter"]->set<int>(500);
-  (*uniform)["colors"]->set<glm::vec3>({20.0f, 100.0f, 5.0f});
-  (*uniform)["world_pos"]->set<glm::vec2>({20.0f, 0.0f});
-  (*uniform)["zoom"]->set<float>(3.0f);
-  (*uniform)["window_ratio"]->set<glm::vec2>({
-    window->getWindowData()->width,
-    window->getWindowData()->height
-  });
-
+  for(float el : data)
+    printf("%.f, ", el);
   
-  // create viewport mesh
-  CW::Renderer::Mesh viewport = CW::Renderer::Mesh(
-  {
-    -1.0f,  1.0f, 0.0f,
-    -1.0f, -1.0f, 0.0f,
-    1.0f,  1.0f, 0.0f,
-    1.0f, -1.0f, 0.0f,
-  }, 
-  {
-    0, 1, 2,
-    1, 3, 2
-  });
 
-  // main loop
-  while(window->getWindowData()->should_close){
-    window->beginFrame();
-
-    malgenbrot->bind();
-    viewport.render();
-    malgenbrot->unbind();
-
-  (*uniform)["window_ratio"]->set<glm::vec2>({
-    window->getWindowData()->width,
-    window->getWindowData()->height
-  });
-
-
-    if(window->getInputData()->right_mouse_button_is_down){
-      (*uniform)["z"]->set<glm::vec2>({
-        3 * (window->getWindowData()->width / 2 - window->getInputData()->mouse_x) / window->getWindowData()->width, 
-        3 * (window->getWindowData()->height / 2 - window->getInputData()->mouse_y) / window->getWindowData()->height
-      });
-    }
-
-    if(window->getInputData()->scroll_is_down){
-      (*uniform)["world_pos"]->set<glm::vec2>({
-        last_world_pos.x - (window->getInputData()->mouse_x - last_mouse_pos.x) * (*uniform)["zoom"]->get<float>(),
-        last_world_pos.y + (window->getInputData()->mouse_y - last_mouse_pos.y) * (*uniform)["zoom"]->get<float>()
-      });
-    }
-    else{
-      last_world_pos = (*uniform)["world_pos"]->get<glm::vec2>();
-      last_mouse_pos = {window->getInputData()->mouse_x, window->getInputData()->mouse_y};
-    };
-
-
-    float zoom = (*uniform)["zoom"]->get<float>();
-    zoom += window->getInputData()->scroll_y * scroll_sensitivity * zoom;
-    zoom = glm::clamp(zoom, 0.000001f, 10.0f);
-    (*uniform)["zoom"]->set<float>(zoom);
-
-    gui->render();
-    window->windowEvents();
-    window->swapBuffer();
-  };
-
-  // clean up
-  delete uniform;
-  delete malgenbrot;
-  delete gui;
+  delete compute;
   delete window;
   
   return 0;
