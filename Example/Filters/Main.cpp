3b6f4a5113f613acd3318f60967ca9f796ef8a81
Daynlight <danielstodulski000@gmail.com>
2025-11-28 17:18:30 +0100

    Image

--- Example/Filters/Main.cpp
+++ Example/Filters/Main.cpp
@@ -1,7 +1,7 @@
 #include "Renderer.h"
 #include "Gui.h"
 
-#include "Shader.h"
+#include "Shaders.h"
 
 
 inline std::function<void(CW::Renderer::iRenderer *window)> settings(){
@@ -16,6 +16,10 @@ int main(){
   CW::Gui::Gui gui(&renderer);
   gui.addWindow("Settings", settings());
 
+  CW::Renderer::Texture texture;
+  if(texture.load("image.png"))
+    return -1;
+
   CW::Renderer::Mesh viewport = CW::Renderer::Mesh(
   {
     -1.0f,  1.0f, 0.0f,
@@ -28,9 +32,30 @@ int main(){
     1, 3, 2
   });
 
+  viewport.addTextCords(
+  {
+    0.0f, 1.0f,
+    0.0f, 0.0f,
+    1.0f, 1.0f,
+    1.0f, 0.0f
+  });
+
+  CW::Renderer::DrawShader texture_shader(Texture::vertex, Texture::fragment);
+  CW::Renderer::Uniform uniform;
+  texture_shader.getUniforms().emplace_back(&uniform);
+  uniform["uTexture"]->set<int>(0);
+
+
 
   while (!renderer.getWindowData()->should_close){
     renderer.beginFrame();
+    
+    texture.bind();
+    texture_shader.bind();
+    viewport.render();
+    texture_shader.unbind();
+    texture.unbind();
+    
     gui.render();
     
     renderer.swapBuffer();
